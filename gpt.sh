85f296776f147ee6f52b3ba2d34cd012619f4a2d
Evgenii <780193464@wwpager.ru>
2024-09-15 19:25:14 +0200

    📝 Added a system description to gpt.sh script.

--- gpt.sh
+++ gpt.sh
@@ -154,7 +154,6 @@ generate_message() {
 
   get_diff_content
 
-
   # Prepare the data for the API call
   SYSTEM_PROMPT="You are a system that generates git commit messages from diff.
   You will be given a diff and your task is to generate a git commit message.
